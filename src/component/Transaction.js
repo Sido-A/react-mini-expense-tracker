e8522084739c66e6fe146eb0cd13c17ea79b5f25
Sido-A <sidonie.abe.uk@gmail.com>
2020-07-14 02:25:36 +0100

    add js logics, add and delete transaction

--- src/component/Transaction.js
+++ src/component/Transaction.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Transaction = ({ transaction }) => {
+  const {deleteTransaction} = useContext(GlobalContext)
   const sign = transaction.amount < 0 ? "-" : "+";
   const plusOrMinus = transaction.amount < 0 ? "minus" : "plus";
+  const id = transaction.id
   return (
     <li className={plusOrMinus}>
       <span>{transaction.text}</span>
       <span>
         {sign}£{Math.abs(transaction.amount)}
       </span>
-      <button>x</button>
+      <button onClick={()=>deleteTransaction(id)}>x</button>
     </li>
   );
 };
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,11 +2,7 @@ import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: +2000 },
-    { id: 3, text: "Mcdonalds", amount: -20 },
-  ],
+  transactions: [],
 };
 
 // Create context
@@ -16,8 +12,27 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  };
+
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  };
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
